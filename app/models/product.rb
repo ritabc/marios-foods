541e2e7619e4ce8f2300356b5bbb05e6fa0d4375
ritabc <ritabc@gmail.com>
2018-09-07 12:28:45 -0700

    Add tests, some pass, but some (review validations of presence of) don't

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :country_of_origin, presence: true
+  validates :name, uniqueness: true
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Product do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
+  it { should have_many :reviews }
+  it { should validate_uniqueness_of :name }
+end
