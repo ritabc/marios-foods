85e955d09f39d8f3161aad37322abce29c4e6727
ritabc <ritabc@gmail.com>
2018-09-07 16:31:10 -0700

    compose index page with about, new products, and most reviewed product sections

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @most_recent_products = Product.three_most_recent
+    @most_reviewed_product = Product.most_reviews[0]
   end
 end
