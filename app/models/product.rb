7eeeaa69ccfd52a68010ccf90dc5945f19c75074
ritabc <ritabc@gmail.com>
2018-09-07 14:51:28 -0700

    Add two scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :country_of_origin, presence: true
   validates :name, uniqueness: true
+
+  scope :most_reviews, -> {(
+    ## find product where product.reviews.count is highest
+    select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
+
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 
 Product.destroy_all
 50.times do |index|
-  Product.create!(name: Faker::Food.unique.ingredient, cost: Faker::Number.normal(15, 15).round(2), country_of_origin: Faker::Address.country)
+  product = Product.create!(name: Faker::Food.unique.ingredient, cost: Faker::Number.normal(15, 5).round(2), country_of_origin: Faker::Address.country)
+  product.update_attributes(created_at: Faker::Date.between(1.year.ago, Date.today))
 end
 
 Review.destroy_all
 250.times do |index|
-  Review.create!(author: Faker::Pokemon.name, content_body: Faker::Lorem.paragraph_by_chars(rand(50..250)), rating: rand(1..5), product_id: Product.all.shuffle.first.id)
+  review = Review.create!(author: Faker::Pokemon.name, content_body: Faker::Lorem.paragraph_by_chars(rand(50..250)), rating: rand(1..5), product_id: Product.all.shuffle.first.id)
+  product = Product.find(review.product_id)
+  review.update_attributes(created_at: Faker::Date.between(product.created_at, Date.today))
 end
-
 
 puts "Created #{Product.count} products"
 puts "Created #{Review.count} reviews"
